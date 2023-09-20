ac6c4d5ed044efe4b84b05ed6e7c6d948902e26f
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-20 00:14:11 -0400

    HW02A
    Recreate one of the given Lygia Clark images as accurately as possible.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
 function setup() {
+        createCanvas (300,300);
+        background (255, 255, 255);
 }
 
 function draw() {
+    fill(0, 0, 0);
+    rect(0, 0, 300, 300);
+    fill(248, 228, 208);
+triangle(0, 0, 300, 0, 200, 150);
+triangle(0, 100, 0, 300, 133, 100);
+triangle(300, 0, 300, 200, 141, 206);
+triangle(0, 300, 300, 300, 86, 171);
 }
